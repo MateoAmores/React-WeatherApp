422b378b4536071e106f2c01ea5dc46bf7c250b5
mateoamores93@hotmail.com <mateoamores93@hotmail.com>
2022-09-13 13:12:37 -0500

    weather app ready

---
+++ src/components/weatherMainInfo.js
@@ -0,0 +1,35 @@
+import styles from "./weatherMainInfo.module.css";
+
+export default function WeatherMainInfo({ weather }) {
+  return (
+    <div className={styles.mainInfo}>
+      <div className={styles.city}>{weather?.location.name}</div>
+      <div className={styles.country}>{weather?.location.country}</div>
+      <div className={styles.row}>
+        <div>
+          <img
+            src={`http:${weather?.current.condition.icon}`}
+            width="128"
+            alt={weather?.current.condition.text}
+          />
+        </div>
+        <div className={styles.weatherCondition}>
+          <div className={styles.condition}>
+            {weather?.current.condition.text}
+          </div>
+          <div className={styles.current}>{weather?.current.temp_c}°</div>
+        </div>
+      </div>
+      <iframe
+        title="mapa"
+        src={`https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15959.25700148108!2d${weather?.location.lon}5!3d${weather?.location.lat}5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sec!4v1663088930163!5m2!1ses-419!2sec`}
+        width="100%"
+        height="450"
+        style={{ border: 0 }}
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+  );
+}
